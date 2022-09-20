99f7f6548bca18930e70afbe6ea3fbdbee177ffe
ly <101566094+HUST-SE-LY@users.noreply.github.com>
2022-09-20 23:13:33 +0800

    fix share bug

--- demo/pages/index/index.js
+++ demo/pages/index/index.js
@@ -25,8 +25,15 @@ Page({
     return {
       title:"不期而喻",
       desc:"来找找你想要的东西吧",
-      path:`/pages/index/itemDetail/itemDetail?id=${e.target.dataset.id}&content=${e.target.dataset.content}&username=${e.target.dataset.username}&price=${e.target.dataset.price}&title=${e.target.dataset.title}&time=${e.target.dataset.time}&location=${e.target.dataset.location}&isFollow=${e.target.dataset.isfollow}&isThumb=${e.target.dataset.isthumb}&isReplied=${e.target.dataset.isreplied}&follow=${e.target.dataset.follow}&reply=${e.target.dataset.reply}&thumb=${e.target.dataset.thumb}&qq=${e.target.dataset.qq}&wx=${e.target.dataset.wx}&avatar=${e.target.dataset.avatar}`
+      path:`/pages/index/index?id=${e.target.dataset.id}&content=${e.target.dataset.content}&username=${e.target.dataset.username}&price=${e.target.dataset.price}&title=${e.target.dataset.title}&time=${e.target.dataset.time}&location=${e.target.dataset.location}&isFollow=${e.target.dataset.isfollow}&isThumb=${e.target.dataset.isthumb}&isReplied=${e.target.dataset.isreplied}&follow=${e.target.dataset.follow}&reply=${e.target.dataset.reply}&thumb=${e.target.dataset.thumb}&qq=${e.target.dataset.qq}&wx=${e.target.dataset.wx}&avatar=${e.target.dataset.avatar}`
 
+    }
+  },
+  onLoad(e) {
+    if(e.id) {
+      wx.navigateTo({
+        url: `./itemDetail/itemDetail?id=${e.id}&content=${e.content}&username=${e.username}&price=${e.price}&title=${e.title}&time=${e.time}&location=${e.location}&isFollow=${e.isFollow}&isThumb=${e.isThumb}&isReplied=${e.isReplied}&follow=${e.follow}&reply=${e.reply}&thumb=${e.thumb}&qq=${e.qq}&wx=${e.wx}&avatar=${e.avatar}`,
+      })
     }
   }
 })
